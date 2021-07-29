ad4ad2263a14c803764d6e479ba67de23b497753
tomdapchai <vothanhtam2407@gmail.com>
2021-07-29 16:37:12 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 function myFunction() { 
-    var e = document.querySelector('.connect');
+    var e = document.querySelector('.contact');
     var c = window.getComputedStyle(e).backgroundColor;
     if (c === "rgb(207, 196, 255)") {
-    document.querySelector('.connect').style.background = "#ff826f";
+    document.querySelector('.contact').style.background = "#ff826f";
 } else{
-    document.querySelector('.connect').style.background = "#cfc4ff";
+    document.querySelector('.contact').style.background = "#cfc4ff";
 }
 }
