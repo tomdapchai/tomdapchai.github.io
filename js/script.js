6fa11274f43bc7eba08daa42852c44543d901b8e
tomdapchai <vothanhtam2407@gmail.com>
2021-08-09 16:03:55 +0700

    bla bla

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function myFunction() { 
-    var e = document.querySelector('.contact');
-    var c = window.getComputedStyle(e).backgroundColor;
-    if (c === "rgb(207, 196, 255)") {
-    document.querySelector('.contact').style.background = "#ff826f";
-} else{
-    document.querySelector('.contact').style.background = "#cfc4ff";
-}
-}
